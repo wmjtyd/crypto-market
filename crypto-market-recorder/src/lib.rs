c8b0c0513038a9e1167a63c5bb83d14fd7a49afa
oss.jtyd <oss.jtyd@yandex.ru>
2022-06-20 09:19:24 +0800

    add recorder

---
+++ crypto-market-recorder/src/lib.rs
@@ -0,0 +1,6 @@
+pub(crate) mod data;
+pub(crate) mod misc_crawlers;
+pub(crate) mod writers;
+
+pub use misc_crawlers::crawl_other;
+pub use writers::create_writer_threads;
---
+++ crypto-market-recorder/src/writers/file_writer.rs
@@ -0,0 +1,57 @@
+use super::Writer;
+
+use log::*;
+use reopen::Reopen;
+use std::{
+    fs,
+    io::{BufWriter, Error, Write},
+    path::Path,
+};
+
+#[cfg(not(windows))]
+use signal_hook::consts::signal::SIGHUP;
+#[cfg(windows)] // Windows has a very limited set of signals, but make it compile at least :-(
+use signal_hook::consts::signal::SIGINT as SIGHUP;
+
+fn open<P: AsRef<Path>>(p: P) -> Result<BufWriter<fs::File>, Error> {
+    info!("reopen {}", p.as_ref().display());
+    let file = fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .append(true)
+        .open(p)?;
+    Ok(BufWriter::new(file))
+}
+
+pub struct FileWriter {
+    file: Reopen<BufWriter<fs::File>>,
+    path: String,
+}
+
+impl FileWriter {
+    pub fn new(path: &str) -> Self {
+        let path_clone = path.to_string();
+        let file = Reopen::new(Box::new(move || open(&path_clone))).unwrap();
+        // Make sure it gets reopened on SIGHUP
+        file.handle().register_signal(SIGHUP).unwrap();
+
+        FileWriter {
+            file,
+            path: path.to_string(),
+        }
+    }
+}
+
+impl Writer for FileWriter {
+    fn write(&mut self, s: &str) {
+        if let Err(e) = writeln!(self.file, "{}", s) {
+            error!("{}, {}", self.path, e);
+        }
+    }
+
+    fn close(&mut self) {
+        if let Err(e) = self.file.flush() {
+            error!("{}, {}", self.path, e);
+        }
+    }
+}
